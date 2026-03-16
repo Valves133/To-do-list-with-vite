ca054128f372d08589d148fd88daee37b1e965b0
Victor Alves <victor.h.m.alves@gmail.com>
2026-03-17 00:42:41 +0100

    Create Icon component

---
+++ src/components/icon.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+import {cva, type VariantProps} from "class-variance-authority";
+
+export const iconVariants = cva("", {
+  variants: {
+    animate: {
+      false: "",
+      true:"animate-spin"
+    }
+  },
+  defaultVariants: {
+    animate: false
+  }
+})
+
+interface IconProps extends React.ComponentProps<"svg">,
+  VariantProps<typeof iconVariants> {
+  svg: React.FC<React.ComponentProps<"svg">>,
+}
+
+export default function Icon({svg: SvgComponents, animate, className, ...props}: IconProps){
+  return<SvgComponents className={iconVariants({animate, className})} {...props} />
+}
